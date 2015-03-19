6ad6ebf59826d5a009c425aebf4524b05c435d0c
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2015-03-19 15:31:05 +0900

    sample for config options

--- skeleton/config.py
+++ skeleton/config.py
@@ -1,2 +1,22 @@
+from oslo.config import cfg
+
+default_opts = [
+    cfg.StrOpt('foo', default='', help='foo value'),
+]
+
+
+egg_opts = [
+    cfg.IntOpt('spam', default=99, help='spam value'),
+]
+
+
+CONF = cfg.CONF
+CONF.register_opts(default_opts)
+CONF.register_opts(egg_opts, group='egg')
+
+
 def list_opts():
-    return []
+    return [
+        ('DEFAULT', default_opts),
+        ('egg', egg_opts),
+    ]
